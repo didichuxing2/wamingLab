8eefb04b0219ed5855ba0911f944558986f1d8c3
li jianhua <lijianhua@lijianhua.local>
2021-02-16 17:42:28 +0800

    v1.0

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.waming {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.waming to javafx.fxml;
+    exports com.waming;
+}
+*/
